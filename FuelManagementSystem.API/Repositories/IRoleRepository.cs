2910738c947d81d3a89d38711223ac820ac4fcc2
Анастасия Столярова <astolyarova@mirshkaf.local>
2025-11-11 09:23:54 +0300

    Добавить/Изменить: - добавлен контроллер RoleController.cs - добавлены репозитории RoleRepository.cs и IRoleRepository.cs

---
+++ FuelManagementSystem.API/Repositories/IRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using FuelManagementSystem.API.Models;
+
+namespace FuelManagementSystem.API.Repositories
+{
+    public interface IRoleRepository : IRepository<Role>
+    {
+        // Специфичные для Role методы
+        Task<IEnumerable<Role>> GetByNameRoleAsync(string nameRole);
+
+        // Методы для работы с активными записями
+        Task<IEnumerable<Role>> GetAllActiveAsync();
+        Task<Role> GetActiveByIdAsync(int id);
+        Task RestoreAsync(int id);
+    }
+}
